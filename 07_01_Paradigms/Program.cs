ce33390512b5a0f6715822b7d42b7b16de404872
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-20 09:16:30 +0300

    paradigmalareklendi

---
+++ 07_01_Paradigms/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] oddnumbers = { 11, 3, 5, 7, 9 ,11 };
+int[] evennumbers = { 10, 20, 50, 60, 30 ,22 };
+int min = findMin(evennumbers);
+int findMin(int[] arr) { 
+
+
+int min = arr[0];
+for (int i = 0; i < arr.Length; i++)
+	{
+	if(arr[i] < min)
+		{
+			min=arr[i];
+			return min;
+		}
+	 
+
+	}
+	return min;
+}
+Console.WriteLine(min);
+Console.ReadKey();
